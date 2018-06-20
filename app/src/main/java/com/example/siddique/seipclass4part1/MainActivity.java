364bdb247286decd91532a74b74008187f01a3e6
kisorniru <kisorniru@gmail.com>
2018-06-21 02:48:36 +0600

    email and checkbox validation is implemented here

--- app/src/main/java/com/example/siddique/seipclass4part1/MainActivity.java
+++ app/src/main/java/com/example/siddique/seipclass4part1/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -23,15 +26,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    //    Example of a listener
     private RadioGroup radioGroup;
-
     private EditText nameET, ageET, phoneET, emailET;
+    private String email, name, age,phone, date;
+    private TextView checkBoxTV;
     private String gender = "Male";
     private List<String> languages = new ArrayList<>();
-
     private Spinner citySP;
     private String city;
     private Button dateBTN;
@@ -44,11 +47,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        checkBoxTV = findViewById(R.id.checkBoxTV);
         nameET = findViewById(R.id.nameET);
         ageET = findViewById(R.id.ageET);
         phoneET = findViewById(R.id.phoneET);
         emailET = findViewById(R.id.emailET);
         dateBTN = findViewById(R.id.dateBTN);
+
         calendar = Calendar.getInstance(Locale.getDefault());
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
@@ -58,13 +63,11 @@ public class MainActivity extends AppCompatActivity {
         dateBTN.setText(date);
 
         radioGroup = findViewById(R.id.genderRG);
-        // anonymous class implementation
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 RadioButton rb = findViewById(checkedId);
                 gender = rb.getText().toString();
-                // Toast.makeText(MainActivity.this, "Is Checked : "+gender, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -88,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 city = getCities().get(position);
 //                city = parent.getItemAtPosition(position).toString();
-//                Toast.makeText(MainActivity.this, "city : "+city, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -107,31 +109,42 @@ public class MainActivity extends AppCompatActivity {
         if (isChecked)
         {
             languages.add(selectedLanguage);
-            // Toast.makeText(this, "Is Checked : "+selectedLanguage, Toast.LENGTH_SHORT).show();
         }
         else
         {
             languages.remove(selectedLanguage);
-            // Toast.makeText(this, "Is Unchecked : "+selectedLanguage, Toast.LENGTH_SHORT).show();
         }
-        for (String s : languages)
-        {
-            Log.e("TAG : ", "selectLanguages: "+s);
-        }
+        /*
+            for (String s : languages)
+            {
+                Log.e("TAG : ", "selectLanguages: "+s);
+            }
+        */
+    }
+
+    private boolean checkEmail(String email){
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
     }
 
     public void register(View view) {
-        String name = nameET.getText().toString();
-        String age = ageET.getText().toString();
-        String phone = phoneET.getText().toString();
-        String email = emailET.getText().toString();
-        String date = dateBTN.getText().toString();
-        Employee employee = new Employee(name, age, phone, email, gender, languages, city, date);
-        // Toast.makeText(this, "Register", Toast.LENGTH_SHORT).show();
-        // Explicit Intent
-        Intent intent = new Intent(this, registeredActivity.class);
-        intent.putExtra("emp", employee);
-        startActivity(intent);
+        email = emailET.getText().toString();
+        name = nameET.getText().toString();
+        age = ageET.getText().toString();
+        phone = phoneET.getText().toString();
+        date = dateBTN.getText().toString();
+
+        if (languages.isEmpty()){
+            checkBoxTV.setError("Please select your expertise area.");
+        }else if (!checkEmail(email)){
+            emailET.setError("Please type your valid email address.");
+        }
+        else {
+            Employee employee = new Employee(name, age, phone, email, gender, languages, city, date);
+            Intent intent = new Intent(this, registeredActivity.class);
+            intent.putExtra("emp", employee);
+            startActivity(intent);
+        }
     }
 
     private List<String> getCities(){
